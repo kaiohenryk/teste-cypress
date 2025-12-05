9783aa1a0f962dd0c544f078d5109e79fa962396
Caio Carvalho <henryk.kaio@gmail.com>
2025-12-05 09:55:11 -0300

    feat(web): adicionar o teste CT01 - deve cadastrar usuário administrador com sucesso

--- cypress/e2e/web/user-registration.cy.js
+++ cypress/e2e/web/user-registration.cy.js
@@ -1,3 +1,29 @@
-describe('', () => {
-  it('', () => {});
+import loginPage from '../../page/LoginPage';
+import userRegistrationPage from '../../page/UserRegistrationPage';
+
+describe('Cadastro de usuário', () => {
+  beforeEach(() => {
+    cy.visit('/');
+  });
+
+  it('CT01 - Deve cadastrar usuário administrador com sucesso', () => {
+    cy.fixture('web/user').then((userData) => {
+      const randomEmail = `teste+${Date.now()}@gmail.com`;
+
+      loginPage.clickRegister();
+      userRegistrationPage.typeName(userData.name);
+      userRegistrationPage.typeEmail(randomEmail);
+      userRegistrationPage.typePassword(userData.password);
+      userRegistrationPage.selectAdministratorOption();
+      userRegistrationPage.clickRegister();
+
+      cy.get('div.alert.alert-dismissible.alert-primary a.alert-link')
+        .should('be.visible')
+        .and('contain', 'Cadastro realizado com sucesso');
+
+      cy.location('pathname', { timeout: 10000 }).should('contain', 'admin/home');
+
+      cy.get('h1').should('contain', 'Bem Vindo').and('contain', userData.name);
+    });
+  });
 });
--- cypress/page/UserRegistrationPage.js
+++ cypress/page/UserRegistrationPage.js
@@ -1,4 +1,4 @@
-import BasePage from './BasePage';
+import { BasePage } from './BasePage';
 
 class UserRegistrationPage extends BasePage {
   selectors = {
